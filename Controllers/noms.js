028592fb40c4141a022f42252cac33e69cb6d762
Nhi Nguyen <nnguy152@gmail.com>
2018-02-22 11:56:57 -0500

    .

--- Controllers/noms.js
+++ Controllers/noms.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-  Nom.findOne({_id: req.params.id}).then(nomz  => {
+  Nom.findOne({_id: req.params.id}).then(nomz => {
     res.render('Noms/show', nomz)
   })
 })
