32e9a6b64a18491a5750e79c37d2df491fe3d536
Nhi Nguyen <nnguy152@gmail.com>
2018-02-22 11:38:06 -0500

    .

--- Controllers/noms.js
+++ Controllers/noms.js
@@ -3,11 +3,9 @@ const router = express.Router()
 const Nom = require('../Models/Nom')
 
 router.get('/', (req, res) => {
-  Nom.find({}).then(nom => {
-    res.render('Noms/index', {nom})
+  Nom.find({}).then(nomz => {
+    res.render('Noms/index', {nomz})
   })
 })
-
-
 
 module.exports = router
