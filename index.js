cd09e010d84aedc282507226a6ec1b557116df9e
Nhi Nguyen <nnguy152@gmail.com>
2018-02-22 10:33:22 -0500

    added npm installs to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const hbs = require('hbs')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.set('view engine', 'hbs')
+app.use(methodOverride('_method'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.listen(4000, () => {
+  console.log('It work brah')
+})
