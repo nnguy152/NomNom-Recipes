e5bc1f780951768b5278698f631778d2d4d10d17
Nhi Nguyen <nnguy152@gmail.com>
2018-02-23 15:03:55 -0500

    added content to login.hbs

--- Models/Nom.js
+++ Models/Nom.js
@@ -1,4 +1,5 @@
 const mongoose = require('../DB/connection')
+const bcrypt = require('bcrypt-nodejs')
 
 const NomSchema = new mongoose.Schema({
   name: {
@@ -19,6 +20,23 @@ const NomSchema = new mongoose.Schema({
   }
 })
 
+var UserSchema = new mongoose.Schema({
+  local: {
+    email: String,
+    password: String
+  }
+})
+
+UserSchema.methods.encrypt = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
+
+UserSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password)
+}
+
 const Nom = mongoose.model('Nom', NomSchema)
+const User = mongoose.model('User', UserSchema)
 
 module.exports = Nom
+module.exports = User
--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const express = require('express')
 const hbs = require('hbs')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
+const passport = require('passport')
+const session = require('express-session')
+const flash = require('connect-flash')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 const nomsController = require('./Controllers/noms')
-
 const app = express()
+
+app.use(morgan('dev'))
+app.use(cookieParser())
+
 app.set('view engine', 'hbs')
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(flash())
+app.use(session())
+
+require('./Config/passport')(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user
+  next()
+})
 
 app.get('/', (req, res) => {
   res.render('index')
